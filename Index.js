7d6a6d5447de14d73e8c5d326d75118aed4a256d
yahoo100kkk <74747418+yahoo100kkk@users.noreply.github.com>
2022-07-10 17:08:14 -0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+setInterval(function(){
+const date = new Date()
+const Hour = date.getHours() - 12
+const Minute = date.getMinutes()
+const second = date.getSeconds()
+const datenow = date.getDate()
+let Am = 'AM'
+if (Hour >= 12){
+  Am = 'PM'
+}
+else{
+  Am = 'AM'
+}
+
+time = `${Hour}:${Minute}:${second}:${Am}`
+  document.querySelector("#txt").innerHTML = time
+},1000)
+
+document.querySelector('button').onclick = () => {
+  if(document.querySelector('body').style.background === 'white'){
+    document.querySelector('body').style.background = 'black';
+    document.querySelector('#txt').style.color = 'white';
+  }
+  else{
+    document.querySelector('body').style.background = "white";
+    document.querySelector('#txt').style.color = 'black';
+    document.querySelector('button').innerHTML = "Light";
+    document.querySelector('button').style.background = "white";
+    document.querySelector('button').style.color = "black";
+  }
+}
+  
+  
+})  
+  
+  
+  
+  
