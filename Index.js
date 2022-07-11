3a6739de36b41b37c64893f318e51a26ac8d1a74
yahoo100kkk <74747418+yahoo100kkk@users.noreply.github.com>
2022-07-11 08:26:05 -0500

    Update Index.js
    Meh

--- Index.js
+++ Index.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 setInterval(function(){
 const date = new Date()
-const Hour = date.getHours() - 12
+var Hour = date.getHours()
 const Minute = date.getMinutes()
 const second = date.getSeconds()
 const datenow = date.getDate()
 let Am = 'AM'
 if (Hour >= 12){
+  Hour = Hour-12
   Am = 'PM'
 }
 else{
@@ -15,23 +16,23 @@ else{
 
 time = `${Hour}:${Minute}:${second}:${Am}`
   document.querySelector("#txt").innerHTML = time
-},1000)
-
-document.querySelector('button').onclick = () => {
-  if(document.querySelector('body').style.background === 'white'){
-    document.querySelector('body').style.background = 'black';
+},1000);
+const button =document.querySelector('button');
+const body =document.querySelector('body');
+button.onclick = () => {
+  if(body.style.background === 'white'){
+    body.style.background = 'black';
     document.querySelector('#txt').style.color = 'white';
-    document.querySelector('button').innerHTML = "Light";
-    document.querySelector('button').style.background = "white";
-    document.querySelector('button').style.color = "black";
+    button.style.background = "white";
+    button.style.color = "black";
+    button.innerHTML = "Light";
   }
   else{
-    document.querySelector('body').style.background = "white";
+    body.style.background = "white";
     document.querySelector('#txt').style.color = 'black';
-    
-document.querySelector('button').style.background = "black";
-    document.querySelector('button').innerHTML = "Dark";
-    document.querySelector('button').style.color = "white";
+    button.innerHTML = "Dark";
+    button.style.background = "black";
+    button.style.color = "white";
   }
 }
   
