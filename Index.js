2d41e302595e7a41ba0b6b85a4356146a3110168
yahoo100k <rajapple876@gmail.com>
2022-09-16 13:54:57 -0500

    shorten lines of codes in function

--- Index.js
+++ Index.js
@@ -1,20 +1,5 @@
-
 setInterval(function(){
-const date = new Date()
-var Hour = date.getHours()
-const Minute = date.getMinutes()
-const second = date.getSeconds()
-const datenow = date.getDate()
-let Am = 'AM'
-if (Hour >= 12){
-  Hour = Hour-12
-  Am = 'PM'
-}
-else{
-  Am = 'AM'
-}
-
-time = `${Hour}:${Minute}:${second}:${Am}`
+const time = new Date().toLocaleTimeString('en-JM')
   document.querySelector("#txt").innerHTML = time
 },1000);
   
@@ -22,15 +7,7 @@ const body = document.getElementById('body')
 const button = document.getElementById('changeBackground')
 
 function change(){
-	body.classList.toggle('dark');
+	body.classList.toggle('light');
 	button.innerHTML === 'Light' ? button.innerHTML = 'Dark' : button.innerHTML = 'Light';
-button.style.backgroundColor === 'white' ? button.style.backgroundColor = 'black' : button.style.backgroundColor = 'white';
-button.style.color === 'black' ? button.style.color = 'white' : button.style.color = 'black';
-  console.log('change');
+	button.classList.toggle('btn-dark')
 }
-
-
-  
-  
-  
-  
